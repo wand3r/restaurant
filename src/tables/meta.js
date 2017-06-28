7bcaf51602aea8af5eaf4887c30325503377937c
Wojciech Karas <wkaras89@gmail.com>
2017-06-29 00:37:23 +0200

    fix runtime errors

--- src/tables/meta.js
+++ src/tables/meta.js
@@ -1,149 +1,166 @@
-import uuid from 'uuid'
-import * as R from 'ramda'
-import * as Num from '../utils/number'
-import * as Str from '../utils/string'
-import * as Obj from '../utils/object'
+import uuid from "uuid";
+import * as R from "ramda";
+import * as Num from "../utils/number";
+import * as Str from "../utils/string";
+import * as Obj from "../utils/object";
 
-const ADD_TABLE = "ADD_TABLE"
-const REMOVE_TABLE = "REMOVE_TABLE"
-const CHANGE_TABLE_NAME = 'CHANGE_TABLE_NAME'
-const CHANGE_TABLE_DIMENSIONS = 'CHANGE_TABLE_DIMENSIONS'
-const MOUSEUP = 'MOUSEUP'
-const MOUSEDOWN = 'MOUSEDOWN'
-const MOUSEMOVE = 'MOUSEMOVE'
+const ADD_TABLE = "ADD_TABLE";
+const REMOVE_TABLE = "REMOVE_TABLE";
+const CHANGE_TABLE_NAME = "CHANGE_TABLE_NAME";
+const CHANGE_TABLE_DIMENSIONS = "CHANGE_TABLE_DIMENSIONS";
+const MOUSEUP = "MOUSEUP";
+const MOUSEDOWN = "MOUSEDOWN";
+const MOUSEMOVE = "MOUSEMOVE";
 
-export const addTable = ({name, length, width}) => ({
+export const addTable = ({ name, length, width }) => ({
   type: ADD_TABLE,
   id: uuid.v4(),
   table: { name, length, width },
-})
-export const removeTable = (id) => ({
+});
+export const removeTable = id => ({
   type: REMOVE_TABLE,
   id,
-})
-export const changeTableName = ({id, name}) => ({
+});
+export const changeTableName = ({ id, name }) => ({
   type: CHANGE_TABLE_NAME,
-  id, name,
-})
-export const changeTableDimensions = ({id, length, width}) => ({
+  id,
+  name,
+});
+export const changeTableDimensions = ({ id, length, width }) => ({
   type: CHANGE_TABLE_DIMENSIONS,
-  id, length, width,
-})
-export const mouseDown = (roomId,coordinates) => ({
+  id,
+  length,
+  width,
+});
+export const mouseDown = (roomId, coordinates) => ({
   type: MOUSEDOWN,
-  roomId, coordinates,
-})
+  roomId,
+  coordinates,
+});
 export const mouseUp = (roomId, coordinates) => ({
   type: MOUSEUP,
-   roomId, coordinates,
-})
+  roomId,
+  coordinates,
+});
 export const mouseMove = (roomId, coordinates) => ({
   type: MOUSEMOVE,
-  roomId, coordinates,
-})
+  roomId,
+  coordinates,
+});
 
 export const tablesReducer = (state = {}, action) => ({
   all: allReducer(state.all, action),
   selectedId: selectedIdReducer(state.selectedId, action, state.all),
-})
+});
 
 export const selectedIdReducer = (state = undefined, action, all) => {
-  switch(action.type) {
+  switch (action.type) {
     case MOUSEUP: {
-      const { roomId, coordinates } = action
+      const { roomId, coordinates } = action;
       const clickedTableId = R.pipe(
-          tableInRoomOnCoordinates(roomId, coordinates),
-          R.keys,
-          R.head,
-        )(all)
-      return clickedTableId
+        tableInRoomOnCoordinates(roomId, coordinates),
+        R.keys,
+        R.head,
+      )(all);
+      return clickedTableId;
     }
     default:
-      return state
+      return state;
   }
-}
+};
 
 export const allReducer = (state = {}, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case ADD_TABLE: {
-      const { id, table } = action
+      const { id, table } = action;
       return {
         ...state,
         [id]: table,
-      }
+      };
     }
     case REMOVE_TABLE: {
-      const { id } = action
-      return R.pickBy((value, key) => key != id, state)
+      const { id } = action;
+      return R.pickBy((value, key) => key != id, state);
     }
     case CHANGE_TABLE_NAME: {
-      const { id, name } = action
+      const { id, name } = action;
       return {
         ...state,
-        [id] : {
+        [id]: {
           ...state[id],
           name,
-        }
-      }
+        },
+      };
     }
     case CHANGE_TABLE_DIMENSIONS: {
-      const { id, length, width } = action
+      const { id, length, width } = action;
       return {
         ...state,
         [id]: {
           ...state[id],
           length,
           width,
-        }
-      }
+        },
+      };
     }
     default:
-      return state
+      return state;
   }
-}
+};
 
-export const tabels => state.tabels
-export const all = state => state.tables.all
-export const ids = Object.keys
-export const withId = R.curry((id, xs) => xs[id])
-export const selected = state => state.tables.all[state.tables.selectedId]
+export const tabels = () => state.tabels;
+export const all = state => state.tables.all;
+export const ids = Object.keys;
+export const withId = R.curry((id, xs) => xs[id]);
+export const selected = state => state.tables.all[state.tables.selectedId];
 export const inRoom = R.curry((roomId, tables) =>
-  R.pickBy(R.pipe(
-    R.path(['position','roomId']),
-    R.equals(roomId)
-  ))(tables))
+  R.pickBy(R.pipe(R.path(["position", "roomId"]), R.equals(roomId)))(tables),
+);
 export const onCoordinates = R.curry((coordinates, tables) =>
-  R.pickBy(R.pipe(
-    toRect,
-    isPointInsideRect(coordinates),
-  ))(tables))
+  R.pickBy(R.pipe(toRect, isPointInsideRect(coordinates)))(tables),
+);
 export const toRect = table => ({
-    x: table.position.x,
-    y: table.position.y,
-    width: table.length,
-    height: table.width,
-  })
+  x: table.position.x,
+  y: table.position.y,
+  width: table.length,
+  height: table.width,
+});
 const inRoomOnCoordinates = R.curry((roomId, coordinates, tables) =>
-  R.pipe(
-    inRoom(roomId),
-    onCoordinates(coordinates)
-  )(tables))
-export const isPointInsideRect = R.curry(({x,y}, rect) => {
-  return x > rect.x && x < rect.x + rect.width
-    && y > rect.y && y < rect.y + rect.height
-})
+  R.pipe(inRoom(roomId), onCoordinates(coordinates))(tables),
+);
+export const isPointInsideRect = R.curry(({ x, y }, rect) => {
+  return (
+    x > rect.x &&
+    x < rect.x + rect.width &&
+    y > rect.y &&
+    y < rect.y + rect.height
+  );
+});
 
-export const validateDimension = (value) => {
-  return value == undefined  ? { isValid: false, msg: "Wymiar stołu nie może być pusty" } :
-         Number.isNaN(value) ? { isValid: false, msg: "Wymiar stołu musi być prawidłową liczbą"} :
-         value <= 0          ? { isValid: false, msg: "Wymiar stołu musi być większy od zera" } :
-         value >= 10         ? { isValid: false, msg: "Wymier stołu jest musi być mniejszy niż 10 metrów" } :
-                               { isValid: true }
-}
+export const validateDimension = value => {
+  return value == undefined
+    ? { isValid: false, msg: "Wymiar stołu nie może być pusty" }
+    : Number.isNaN(value)
+      ? { isValid: false, msg: "Wymiar stołu musi być prawidłową liczbą" }
+      : value <= 0
+        ? { isValid: false, msg: "Wymiar stołu musi być większy od zera" }
+        : value >= 10
+          ? {
+              isValid: false,
+              msg: "Wymier stołu jest musi być mniejszy niż 10 metrów",
+            }
+          : { isValid: true };
+};
 
-export const validateName = ({id, name}, tables) => {
-  return Str.isEmpty(name)                                                ? { isValid: false, msg: "Nazwa stołu nie może być pusty" } :
-         Str.length(name) > 10                                            ? { isValid: false, msg: "Nazwa stołu nie może być dłuższy niż 10 znaków" } :
-         R.pipe(Obj.withoutKey(id), Obj.any(x => x.name == name))(tables) ? { isValid: false, msg: "Nazwa stół jest już zajęta" } :
-                                                                             { isValid: true }
-}
+export const validateName = ({ id, name }, tables) => {
+  return Str.isEmpty(name)
+    ? { isValid: false, msg: "Nazwa stołu nie może być pusty" }
+    : Str.length(name) > 10
+      ? {
+          isValid: false,
+          msg: "Nazwa stołu nie może być dłuższy niż 10 znaków",
+        }
+      : R.pipe(Obj.withoutKey(id), Obj.any(x => x.name == name))(tables)
+        ? { isValid: false, msg: "Nazwa stół jest już zajęta" }
+        : { isValid: true };
+};
--- src/tables/table.jsx
+++ src/tables/table.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { ListItem, IconButton, Card } from 'semantic-react'
-import { RemoveButton } from '../components/buttons'
-import * as F from './../form/field'
-import * as M from './meta'
-import * as R from 'ramda'
+import React from "react";
+import { connect } from "react-redux";
+import { ListItem, IconButton, Card } from "semantic-react";
+import { RemoveButton } from "../components/buttons";
+import * as F from "./../form/field";
+import * as M from "./meta";
+import * as R from "ramda";
 
 const Table = ({
   id,
-  table: {name, length, width},
+  table: { name, length, width },
   tables,
   changeName,
   changeDimensions,
   remove,
-}) => (
-  <ListItem style={{padding: '3px 0px'}}>
-    <Card fluid style={{padding: '3px 5px'}}>
-      <div style={{display: 'flex', flexDirection: 'row'}}>
-        <div style={{flex: '1'}}>
+}) =>
+  <ListItem style={{ padding: "3px 0px" }}>
+    <Card fluid style={{ padding: "3px 5px" }}>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <div style={{ flex: "1" }}>
           <F.IndependentField
             header="Nazwa"
             type="text"
             autoFocus={true}
             initialValue={name}
-            validator={name => M.validateName({id, name}, tables)}
-            onSave={name => changeName({id, name})}
+            validator={name => M.validateName({ id, name }, tables)}
+            onSave={name => changeName({ id, name })}
           />
           <F.IndependentField
             header="Długość"
@@ -34,7 +34,7 @@ const Table = ({
             autoFocus="true"
             initialValue={length}
             validator={M.validateDimension}
-            onSave={length => changeDimensions({id, length, width})}
+            onSave={length => changeDimensions({ id, length, width })}
           />
           <F.IndependentField
             header="Szerokość"
@@ -44,25 +44,29 @@ const Table = ({
             autoFocus="true"
             initialValue={width}
             validator={M.validateDimension}
-            onSave={width => changeDimensions({id, length, width})}
+            onSave={width => changeDimensions({ id, length, width })}
           />
         </div>
-        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
-          <RemoveButton
-            onClick={() => remove(table.id)}
-          />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <RemoveButton onClick={() => remove(id)} />
         </div>
       </div>
     </Card>
-  </ListItem>
-)
+  </ListItem>;
 export default connect(
-  (state, {id}) => ({
+  (state, { id }) => ({
     tables: M.all(state),
-    table: R.pipe(M.all,M.withId(id))(state),
-  }), {
+    table: R.pipe(M.all, M.withId(id))(state),
+  }),
+  {
     changeName: M.changeTableName,
     changeDimensions: M.changeTableDimensions,
-    remove: M.removeTable
-  }
-)(Table)
+    remove: M.removeTable,
+  },
+)(Table);
